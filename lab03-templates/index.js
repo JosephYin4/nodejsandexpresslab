edbdab52a1e9e68fbc8266cc0834c9ca433fcd88
JosephYin4 Yin <yincc147@hotmail.com>
2024-10-17 03:34:20 +0000

    minor updates

--- lab03-templates/index.js
+++ lab03-templates/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const hbs = require('hbs'); // <-- NOTE 1
 
 /* 1. SETUP EXPRESS */
 let app = express();
-const hbs = require('hbs'); // <-- NOTE 1
 
 // 1B. SETUP VIEW ENGINE
 app.set('view engine', 'hbs'); // <-- NOTE 2
 
 // add routes here
+app.get('/', function(req,res){
+    res.send("<h1>Hello from Express</h1>");
+    })
+
+    app.get('/hello/:name', (req,res)=>{
+        let name = req.params.name;
+        res.send("Hi, " + name);
+        })
 
         app.get('/', function(req, res){
             res.render('index.hbs');
