fd4bb046c252064ec42d276c45888e6468dd89b4
JosephYin4 <yincc147@hotmail.com>
2024-10-17 07:44:16 +0000

    minor updates

--- lab03-templates/index.js
+++ lab03-templates/index.js
@@ -7,11 +7,14 @@ let app = express();
 // 1B. SETUP VIEW ENGINE
 app.set('view engine', 'hbs'); // <-- NOTE 2
 
+// 1C. SETUP STATIC FOLDER
+app.use(express.static('public'));
+
 // add routes here
 
-        app.get('/', function(req, res){
-            res.render('index.hbs');
-            })
+ //       app.get('/', function(req, res){
+ //           res.render('index.hbs');
+ //           })
 
 app.listen(3000, ()=>{
     console.log("Server started")
--- public/index.js
+++
@@ -1,6 +0,0 @@
-/* 1. SETUP EXPRESS */
-let app = express();
-// 1B. SETUP VIEW ENGINE
-app.set('view engine', 'hbs');
-// 1C. SETUP STATIC FOLDER
-app.use(express.static('public'));
