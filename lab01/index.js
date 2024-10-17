a59b53a44b3a8b50078a427da2e352d2e38d3b36
JosephYin4 Yin <yincc147@hotmail.com>
2024-10-17 03:07:50 +0000

    minor updates

--- lab01/index.js
+++ lab01/index.js
@@ -3,6 +3,14 @@ const express = require('express');
 let app = express();
 
 // add routes here
+app.get('/', function(req,res){
+    res.send("<h1>Hello from Express</h1>");
+    })
+
+    app.get('/hello/:name', (req,res)=>{
+        let name = req.params.name;
+        res.send("Hi, " + name);
+        })
 
 app.listen(3000, ()=>{
     console.log("Server started")
